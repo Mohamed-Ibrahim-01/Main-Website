15592de59bcf8f60f3188fc1414fc629dd48dfcf
habiba mohamed <hobyhabiba@gmail.com>
2021-07-17 22:13:05 +0200

    added sponsors section

--- client/src/containers/Sponsors.js
+++ client/src/containers/Sponsors.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import Sponsors from '../components/sponsors/indexs'
+import Logo1 from '../images/semlogo.png'
+import Logo2 from '../images/ieeelogo.png'
+import Logo3 from '../images/emblogo.png'
+import Logo4 from '../images/nekhlogo.png'
+import Logo5 from '../images/megalogo.jpg'
 
-function Sponsors() {
+
+export function SponsorsContainer() {
     return (
-        <div>
-            
-        </div>
+        <Sponsors>
+            <Sponsors.Title>Previous Sponsors</Sponsors.Title>
+                        <Sponsors.Link href="/home">  
+                        </Sponsors.Link>
+           <Sponsors.Logo  src={Logo1} alt="logo" /> 
+           <Sponsors.Logo  src={Logo2} alt="logo" /> 
+           <Sponsors.Logo  src={Logo3} alt="logo" /> 
+           <Sponsors.Logo  src={Logo4} alt="logo" /> 
+           <Sponsors.Logo1  src={Logo5} alt="logo" />  
+        </Sponsors>
     )
 }
 
-export default Sponsors
+export default SponsorsContainer
+
