c93c3bc42e4063ee1ac03c322def8919f21f81b8
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2021-07-18 01:27:27 +0200

    missed data in magazine

---
+++ client/src/containers/data/magazine.js
@@ -0,0 +1,30 @@
+import research from '../../images/research.png'
+import technology from '../../images/cpu.png'
+import engineering from '../../images/medical-report.png'
+const features = [
+    {
+        Title: 'Biomedical Engineering',
+        Image: engineering,
+        Description: 'A lot of interesting articles about biomedical engineering fields',
+    },
+
+    {
+        Title: 'New Technology',
+        Image: technology,
+        Description: 'Discover the latest technology in biomedical engineering',
+    },
+
+    {
+        Title: 'Latest Researchs & Projects',
+        Image: research,
+        Description: 'Explore the latest researchs and projects in biomedical engineering',
+    }
+]
+
+const title = "BEAT Magazine"
+
+const description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+
+export const magazine = {
+  title,features,description
+}
