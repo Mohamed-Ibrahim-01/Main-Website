5f35a765b6a103506e6e45f06228e33de8d39046
Mohamed Ibrahim <mohamed.ahmed002023@gmail.com>
2021-07-19 01:57:06 +0200

    contact us mail sending

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const createError = require('http-errors')
 const express = require('express');
 const logger = require('morgan')
+const nodemailer = require('nodemailer')
 
 const Article = require('./models/article.js');
 const Contact = require('./models/contact.js');
@@ -10,6 +11,7 @@ const Magazine = require('./models/magazine.js');
 
 require('dotenv').config()
 
+const { PORT , DB_URI, WEBSITE_MAIL, TEST_MAIL, WEBSITE_MAIL_PASS} = process.env;
 
 //import indexRouter = require('./routes/index');
 
@@ -26,9 +28,8 @@ app.set('view engine', 'ejs');
 
 //connect to mongodb
 
-
-mongoose.connect(dbURI, { useNewUrlParser: true,  useUnifiedTopology: true, useFindAndModify: false })
-    .then((result) => app.listen(3000))
+mongoose.connect(DB_URI, { useNewUrlParser: true,  useUnifiedTopology: true, useFindAndModify: false })
+    .then((result) => app.listen(PORT))
     .catch((err) => console.log(err));
 
 
@@ -104,3 +105,45 @@ app.get('/article/', (req, res) => {
        console.log(err);
    })
 })
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: WEBSITE_MAIL,
+        pass: WEBSITE_MAIL_PASS,
+    },
+})
+
+let mailOptions = {
+    from: WEBSITE_MAIL,
+    to: TEST_MAIL,
+    subject: 'Contact-Us Feedback [BEAT Website]',
+    text: `
+        name : ${contact.name},
+        email: ${contact.email},
+        message: ${contact.message}
+    `,
+    html: `
+        <h2 style="text-align:center;  "> Feedback </h2>
+        <hr align=”center” color="#000c26">
+
+        <p style=" font-size: 1.1em"> <b>Name</b>: ${contact.name}</p>
+        <p style="display:inline; font-size: 1.1em"> <b>Email</b>:
+        <p style="display:inline; font-size: 1.1em;">${contact.name} </p>
+
+        <div
+            style=" box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);padding: 16px;text-align: center;background-color: #f1f1f1;max-width: 600px;margin:3vh auto;">
+            <h3 style="color:#2276e7">Message</h1>
+            ${contact.message}
+        </div>
+    `,
+}
+
+transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log('email sent')
+    }
+})
+
