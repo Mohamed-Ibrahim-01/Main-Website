6b89dc8ac4f959f0560bdbaa4ec0235c9ef31fdc
Mohamed Ibrahim <mohamed.ibrahim02023@gmail.com>
2021-07-22 03:26:55 +0200

    fixing identaion

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import {Home, ContactUs, NotFound} from "./components/MyComponents"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Home, ContactUs, NotFound } from './components/MyComponents'
 import { FooterContainer } from './containers/Footer'
 import { NavBarContainer } from './containers/Navbar'
 import { MagazineContainer } from './containers/Magazine'
 
 function App() {
-  return (
-    <Router>
-      <NavBarContainer />
-      <Switch>
-        <Route exact path="/"> <Home /> </Route>
-        <Route path="/ContactUs"> <ContactUs /> </Route>
-        <Route path="/Magazine"> <MagazineContainer /> </Route>
-        <Route path="*"> <NotFound /> </Route>
-      </Switch>
-      <FooterContainer />
-    </Router>
-  );
+    return (
+        <Router>
+            <NavBarContainer />
+            <Switch>
+                <Route exact path="/"> <Home /> </Route>
+                <Route path="/ContactUs"> <ContactUs /> </Route>
+                <Route path="/Magazine"> <MagazineContainer /> </Route>
+                <Route path="*"> <NotFound /> </Route>
+            </Switch>
+            <FooterContainer />
+        </Router>
+    )
 }
 
-export default App;
+export default App
