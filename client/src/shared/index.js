ca0a8abf3dc97cd1c2127833dbed67c7847e6d52
Mohamed Ibrahim <mohamed.ibrahim02023@gmail.com>
2021-07-25 10:25:22 +0200

    adding shared folder

---
+++ client/src/shared/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { Grid } from '@material-ui/core'
+
+export const containerItemGrid = styled(Grid).attrs({
+    container: true,
+    item: true,
+})``
+
