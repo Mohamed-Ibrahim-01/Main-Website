096f54ef745ee4c77958f4f376d9f65164ef5048
Mohamed Ibrahim <mohamed.ibrahim02023@gmail.com>
2021-07-28 00:12:17 +0200

    making the cover under navbar

--- client/src/modules/home/index.js
+++ client/src/modules/home/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 //import {About, Sponsors, Cover} from "./parts"
-import {About} from "./parts"
+import {About, Cover} from "./parts"
 
 export default function Home() {
     return (
         <section>
-            {/*<Cover/>*/}
+            <Cover/>
             <About/>
             {/*<Sponsors/>*/}
         </section>
