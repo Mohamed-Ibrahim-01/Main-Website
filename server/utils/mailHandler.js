a1fc03114719fb722d0806a553d78683e7ea04fe
Mohamed Ibrahim <mohamed.ibrahim02023@gmail.com>
2021-10-12 00:49:58 +0200

    refactor sendFeedback

--- server/utils/mailHandler.js
+++ server/utils/mailHandler.js
@@ -8,33 +8,43 @@ let transporter = nodemailer.createTransport({
     },
 })
 
-let mailOptions = {
-    from: WEBSITE_MAIL,
-    to: TEST_MAIL,
-    subject: 'Contact-Us Feedback [BEAT Website]',
-    text: `
-        name : ${contact.name},
-        email: ${contact.email},
-        message: ${contact.message}
-    `,
-    html: `
-        <h2 style="text-align:center;  "> Feedback </h2>
-        <hr align=”center” color="#000c26">
+function mailOptions(feedback) {
+    styles = 
+        "box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);"+
+        "padding: 16px;"+
+        "text-align: center;"+
+        "background-color: #f1f1f1;"+
+        "max-width: 600px;"+
+        "margin:3vh auto;"
 
-        <p style=" font-size: 1.1em"> <b>Name</b>: ${contact.name}</p>
-        <p style="display:inline; font-size: 1.1em"> <b>Email</b>:
-        <p style="display:inline; font-size: 1.1em;">${contact.name} </p>
+    generatedMail = {
+        from: WEBSITE_MAIL,
+        to: TEST_MAIL,
+        subject: 'Contact-Us Feedback [BEAT Website]',
+        text: `
+            name : ${feedback.name},
+            email: ${feedback.email},
+            message: ${feedback.message}
+        `,
+        html: `
+            <h2 style="text-align:center;  "> Feedback </h2>
+            <hr align=”center” color="#000c26">
 
-        <div
-            style=" box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);padding: 16px;text-align: center;background-color: #f1f1f1;max-width: 600px;margin:3vh auto;">
-            <h3 style="color:#2276e7">Message</h1>
-            ${contact.message}
-        </div>
-    `,
+            <p style=" font-size: 1.1em"> <b>Name</b>: ${feedback.name}</p>
+            <p style="display:inline; font-size: 1.1em"> <b>Email</b>:
+            <p style="display:inline; font-size: 1.1em;">${feedback.name} </p>
+
+            <div
+                style="${styles}">
+                <h3 style="color:#2276e7">Message</h1>
+                ${contact.message}
+            </div>
+        `,
+    }
 }
 
 exports.sendFeedbackMail = async (feedback) => {
-    transporter.sendMail(mailOptions, function (error, info) {
+    transporter.sendMail(mailOptions(feedback), function (error, info) {
         if (error) {
             console.log(error)
         } else {
@@ -42,5 +52,3 @@ exports.sendFeedbackMail = async (feedback) => {
         }
     })
 }
-
-
