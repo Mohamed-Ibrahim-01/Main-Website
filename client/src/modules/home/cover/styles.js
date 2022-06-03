a7f4eef1cfd03193bd23942eb4c5346441251ceb
mohamed-ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2022-06-04 00:48:02 +0200

    fixing cover responsitivity, logo and desciption

--- client/src/modules/home/cover/styles.js
+++ client/src/modules/home/cover/styles.js
@@ -1,11 +1,16 @@
 import styled from "styled-components"
-import background from "./images/cover.jpg"
+import background from "./images/cover_.jpg"
 
 export const Cover = styled.div`
   background-image: url(${background});
-  height: 67vh;
-  background-size:cover;
-  background-position: top right;
+  @media (max-width: 950px) {
+    height: 40vh;
+  }
+  @media (min-width: 950px) {
+    height: 67vh;
+  }
+  background-size: cover;
+  background-position: center center;
   background-repeat: no-repeat;
   margin-top: 60px;
 `
